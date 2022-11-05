875eeed52e5c529a1aff9d3c43bf94bfb9fba0b1
Alexey Kostin <kostin@users.noreply.github.com>
2022-11-06 00:52:01 +0300

    Create backup-vps.sh

---
+++ backup-vps.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DATE=`date "+%Y-%m-%d"`
+BACKUPDIR="/backups/hestia"
+SCRIPTDIR="$(dirname "$0")"
+source "$SCRIPTDIR/servers.sh"
+
+logadd () {
+  echo "$1"
+}
+
+mkdir -p ${BACKUPDIR}
+
+for SRV in "${HESTIAS[@]}"
+do
+  logadd "..."$(date '+%H:%M:%S')" Start working with server ${SRV}"
+  rsync -avz root@${SRV}:/backup/ ${BACKUPDIR}/
+done
